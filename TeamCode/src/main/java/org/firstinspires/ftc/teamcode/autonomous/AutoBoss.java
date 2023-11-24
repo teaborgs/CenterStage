7777beeb32a43c05dadaad7baebcc3dfd02dbb96
Vlad-07 <93938455+Vlad-07@users.noreply.github.com>
2023-11-24 20:40:57 +0200

    Started autonomous work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBoss.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBoss.java
@@ -7,23 +7,30 @@ import static org.firstinspires.ftc.teamcode.Utilities.WaitFor;
 import static org.firstinspires.ftc.teamcode.Utilities.centimetersToInches;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Utilities;
+import org.firstinspires.ftc.teamcode.opencv.TeamPropDetectionPipeline;
 import org.firstinspires.ftc.teamcode.subsystems.impl.ClawSystem;
 import org.firstinspires.ftc.teamcode.subsystems.impl.IntakeSystem;
 import org.firstinspires.ftc.teamcode.subsystems.impl.LiftSystem;
 import org.firstinspires.ftc.teamcode.subsystems.impl.RotatorSystem;
 import org.firstinspires.ftc.teamcode.subsystems.impl.TumblerSystem;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Autonomous(name = "Demo Auto", group = "Auto")
-public class DemoAuto extends LinearOpMode
+@Autonomous(name = "AutoBoss", group = "Auto")
+public class AutoBoss extends LinearOpMode
 {
 	private MecanumDrive mecanumDrive;
 	private TumblerSystem tumblerSystem;
@@ -32,7 +39,8 @@ public class DemoAuto extends LinearOpMode
 	private IntakeSystem intakeSystem;
 	private LiftSystem liftSystem;
 
-	private Utilities.DetectionCase detectionCase = Utilities.DetectionCase.NONE;
+	private OpenCvCamera camera;
+	private TeamPropDetectionPipeline detectionPipeline;
 
 	@Override
 	public void runOpMode()
@@ -44,16 +52,17 @@ public class DemoAuto extends LinearOpMode
 
 	private void Detection()
 	{
-		// todo
-
 		while (!isStarted())
 		{
-			;
+			telemetry.addData("Case: ", detectionPipeline.getDetectionCase().name());
+			telemetry.update();
 		}
 	}
 
 	private void Init()
 	{
+		Constants.Init(Utilities.GetCurrentRobotType(hardwareMap));
+
 		mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
 		liftSystem = new LiftSystem(hardwareMap.get(DcMotorEx.class, "lift1"), hardwareMap.get(DcMotorEx.class, "lift2"));
@@ -70,14 +79,28 @@ public class DemoAuto extends LinearOpMode
 		rotatorSystem.Init();
 		clawSystem.Init();
 
+		int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+		camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+		detectionPipeline = new TeamPropDetectionPipeline();
+		detectionPipeline.setDebug(true); // Comment this out to disable debug mode
+		camera.setPipeline(detectionPipeline);
+		camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+		{
+			@Override
+			public void onOpened()
+			{
+				camera.startStreaming(Constants.Camera.CAMERA_WIDTH, Constants.Camera.CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+			}
+
+			@Override
+			public void onError(int errorCode) { telemetry.addData("Camera Error", errorCode); }
+		});
+
 		telemetry.setMsTransmissionInterval(50);
 	}
 
 	private void Run()
 	{
-		if (detectionCase == Utilities.DetectionCase.NONE)
-			detectionCase = Utilities.DetectionCase.CENTER;
-
 //		Actions.runBlocking(
 //				RunSequentially(
 //						clawSystem.MoveToPosition(Constants.getClawBusy()),
@@ -120,18 +143,28 @@ public class DemoAuto extends LinearOpMode
 //				)
 //		);
 
-		switch (detectionCase)
+		switch (detectionPipeline.getDetectionCase())
 		{
-
 			case LEFT:
 				Actions.runBlocking(RunSequentially(
-						clawSystem.MoveToPosition(Constants.getClawBusy()),
 						RunInParallel(
+								clawSystem.MoveToPosition(Constants.getClawBusy()),
 								mecanumDrive.actionBuilder(mecanumDrive.pose)
-										.lineToX(-centimetersToInches(60))
+										.lineToXSplineHeading(-centimetersToInches(65), Math.PI / 2)
 										.build(),
 								tumblerSystem.MoveToPosition(Constants.getTumblerSpikeMark()),
 								rotatorSystem.MoveToPositionWithDelay(Constants.getRotatorBusy(), 0.1)
+						),
+						WaitFor(0.2),
+						RunInParallel(
+								mecanumDrive.actionBuilder(new Pose2d(-centimetersToInches(65), 0, Math.PI / 2))
+										.waitSeconds(0.2)
+										.lineToYSplineHeading(centimetersToInches(100), -Math.PI / 2)
+										.build(),
+								clawSystem.MoveToPosition(Constants.getClawIdle()),
+								rotatorSystem.MoveToPositionWithDelay(Constants.getRotatorIdle(), 0.5),
+								tumblerSystem.MoveToPositionWithDelay(Constants.getTumblerLoad(), 1),
+								intakeSystem.RunIntakeFor(1.5)
 						)
 				));
 				break;
@@ -147,6 +180,10 @@ public class DemoAuto extends LinearOpMode
 
 
 				break;
+
+			case NONE:
+				telemetry.addLine("Invalid case! (NONE)");
+				telemetry.update();
 		}
 	}
 }
