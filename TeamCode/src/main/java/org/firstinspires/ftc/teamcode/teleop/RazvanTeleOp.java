1eb979a1ac418137e4eb5d858063769af7e63f28
Vlad-07 <vladup07@gmail.com>
2023-12-12 19:57:57 +0200

    Undo TeleOp "fix"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RazvanTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RazvanTeleOp.java
@@ -158,8 +158,8 @@ public final class RazvanTeleOp extends BaseOpMode
 		int modifier = INVERTED ? -1 : 1;
 		double angle = (wheelInput.getValue(Bindings.Wheel.ROTATE_AXIS_L) - wheelInput.getValue(Bindings.Wheel.ROTATE_AXIS_R)) * (turbo ? 0.1 : suppress ? 0.03 : 0.08);
 		Vector2d wheelVel = new Vector2d(
-				wheelInput.getValue(Bindings.Wheel.DRIVE_AXIS_Y) * modifier,
-				wheelInput.getValue(Bindings.Wheel.DRIVE_AXIS_X) * modifier
+				wheelInput.getValue(Bindings.Wheel.DRIVE_AXIS_Y) * modifier * (currentRobot == Utilities.RobotType.ROBOT_2 ? -1 : 1),
+				wheelInput.getValue(Bindings.Wheel.DRIVE_AXIS_X) * modifier * (currentRobot == Utilities.RobotType.ROBOT_2 ? -1 : 1)
 		).times(turbo ? 1.0 : suppress ? 0.3 : 0.8);
 		mecanumDrive.setDrivePowers(new PoseVelocity2d(wheelVel, angle));
 	}
